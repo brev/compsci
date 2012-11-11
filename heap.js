6e13c7271d4075e1765dacbe052cd3876dc1576e
U-Mercurial\Brev <Brev@Mercurial.(none)>
2012-11-11 15:13:23 -0800

    more

--- heap.js
+++ heap.js
@@ -1,17 +1,28 @@
+var util = require('util');
+
 // easy to find max of data
 
 var Heap = function(opts) {
 	this.rootNode = opts.rootNode || null;
+};
+Heap.prototype.fillParents = function(node) {
+	var n = node || this.rootNode;
+
+	if(n.left) n.left.parent = n;
+	if(n.right) n.right.parent = n;
+
+	if(n.left) this.fillParents(n.left);
+	if(n.right) this.fillParents(n.right);
 };
 Heap.prototype.showAll = function(node) {
 	var node = node || this.rootNode;
 	console.log(node.value);
 	if(node.left) this.showAll(node.left);	
 	if(node.right) this.showAll(node.right);	
-}
+};
 Heap.prototype.getMax = function() {
 	return(this.rootNode.value);
-}
+};
 Heap.prototype.getMin = function(node) {
 	var node = node || this.rootNode;	
 	var min = node.value;
@@ -22,19 +33,27 @@ Heap.prototype.getMin = function(node) {
 		min = this.getMin(node.right);
 	}
 	return(min);
-}
+};
 Heap.prototype.insert = function(newNode) {
-	if(newNode.value > this.rootNode.value) {
+	var n = this.rootNode;	
+	while((newNode.value <= n.value) && n.left) {
+		n = n.left;
+	}
+	if(n === this.rootNode) {
 		var oldRoot = this.rootNode;
 		this.rootNode = newNode;
-		newNode.left = null;
-		newNode.right = oldRoot;	
+		newNode.left = oldRoot;
+		newNode.left.parent = newNode;	
 	}
 	else {
+		var oldLeft = n;
+		n.parent.left = newNode;
+		newNode.left = oldLeft;	
 	}
 };
 
 var Node = function(opts) {
+	this.parent = null;	
 	this.value = opts.value || null;
 	this.left = opts.left || null;
 	this.right = opts.right || null;
@@ -65,13 +84,15 @@ var h = new Heap({
 		})
 	})
 });
+h.fillParents();
 
 h.insert(new Node({
 	value: 141,
-	left: null,
-	right: null
 }));
-console.log(h);
+h.insert(new Node({
+	value: 68,
+}));
+console.log(util.inspect(h, true, 20));
 console.log('max', h.getMax());
 console.log('min', h.getMin());
 h.showAll();
